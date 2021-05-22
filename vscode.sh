112f275cd36e9fd443b31a18528342c55d9b33c6
Richie <TheMadMaker2@protonmail.com>
2021-05-22 16:11:14 -0400

    Added Vcode.sh

---
+++ vscode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#setup time zone
+sudo timedatectl set-timezone America/New_York
+sudo systemctl enable systemd-timesyncd
+
+chmod 700 ~/.files/update.sh
+~/.files/update.sh
+#Installs archiving/compreshion Pacages
+sudo pacman -S --needed p7zip unrar tar rsync zstd 
+#Installs basic pacages
+sudo pacman -S --needed base-devel curl wget nano neovim flatpak
+#Installs zsh
+sudo pacman -S --needed zsh && chsh -s /bin/zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+#Installs yay
+cd ~/
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Syu
+
+#installs genral pacages
+yay -S --needed rar jdk cat code-server
+
