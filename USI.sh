b2ad94650cd23d8334ad32a1d13673dff89edce3
Richie <TheMadMaker2@protonmail.com>
2021-05-25 22:58:40 -0400

    worked around echo ssh

--- USI.sh
+++ USI.sh
@@ -27,6 +27,7 @@ if [[ $1 == alice ]]; then
   sudo groupadd alice
   sudo useradd -m -g alice -G users -G sudo alice
   sudo chsh -s /bin/zsh alice
-  sudo mkdir /home/alice/.ssh/ && sudo echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWG3cIBju6vzX6s8JlmGNJOiWY7pQ19bHvcqDADtWzv snowi@DESKTOP-EVIR8IH" > /home/alice/.ssh/authorized_keys
+  echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWG3cIBju6vzX6s8JlmGNJOiWY7pQ19bHvcqDADtWzv snowi@DESKTOP-EVIR8IH" > /tmp/authorized_keys
+  sudo mkdir /home/alice/.ssh/ && sudo mv /tmp/authorized_keys
   sudo chown alice:alice -R /home/alice/
 fi
