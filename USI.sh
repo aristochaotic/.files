562a6561914710ba6501afa093556202f01af36a
Richie <TheMadMaker2@protonmail.com>
2021-05-27 19:08:56 -0400

    Moves zsh install to the end

--- USI.sh
+++ USI.sh
@@ -8,8 +8,6 @@ sudo systemctl enable systemd-timesyncd
 sudo apt install p7zip unrar tar rsync zstd 
 #Installs basic pacages
 sudo apt install curl wget nano neovim
-#Installs zsh
-sudo apt install zsh && chsh -s /bin/zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "#Bob
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG50sgvTSDclHBXiGljCwo9NWfM411U6f12Xog0nt6Bw BOB
@@ -18,12 +16,6 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0J5BnE39FsLimpAAvbrZI4xfGM5Si0UOpTsDwOfITh
 #Phone
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJDhddiEwi77eNYLYPJhfP/VZaUqezTNvxIwh0dV2SS JuiceSSH" > ~/.ssh/authorized_keys
 
-
-#Links zsh and vim rc files
-ln -fs ~/.files/vimrc ~/.vimrc
-ln -fs ~/.files/zshrc ~/.zshrc
-#Removes unneeded files
-rm ~/.bash* ~/.zshrc.pre-oh-my-zsh
 
 if [[ $1 == alice ]]; then
 	sudo groupadd alice
@@ -44,3 +36,13 @@ if [[ $1 == ansible ]]; then
 	sudo mv /tmp/authorized_keys /home/ansible/.ssh/authorized_keys
 	sudo chown ansible:ansible -R /home/ansible/
 fi
+
+#Links zsh and vim rc files
+ln -fs ~/.files/vimrc ~/.vimrc
+ln -fs ~/.files/zshrc ~/.zshrc
+
+#Installs zsh
+sudo apt install zsh && chsh -s /bin/zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Removes unneeded files
+rm ~/.bash* ~/.zshrc.pre-oh-my-zsh
